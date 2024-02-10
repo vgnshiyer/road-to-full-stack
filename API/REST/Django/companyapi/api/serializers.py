d01ebfb7689b79bf5dea52e7fd0d8d0c586eeed1
vgnshiyer <vgnshiyer@gmail.com>
2024-02-10 13:56:02 -0700

    chore: added employee endpoint

--- API/REST/Django/companyapi/api/serializers.py
+++ API/REST/Django/companyapi/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from api.models import Company
+from api.models import Employee
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
         # fields to be included while serializing
+        fields = "__all__"
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    employee_id = serializers.ReadOnlyField()
+    class Meta:
+        model = Employee
         fields = "__all__"
--- API/REST/Django/companyapi/api/urls.py
+++ API/REST/Django/companyapi/api/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CompanyViewSet
+from api.views import CompanyViewSet, EmployeeViewSet
 from rest_framework.routers import DefaultRouter
 
 # creates all http methods (create, retrieve, update, partial_update, destroy) 
 router = DefaultRouter()
 router.register(r'companies', CompanyViewSet)
+router.register(r'employees', EmployeeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
